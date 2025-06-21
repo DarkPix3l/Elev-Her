bbecdc318dac541b56cfc531d318fe8a8951618f
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-06-21 20:57:50 +0200

    feat: moved backend (MongoDB/Express) into Next.js monorepo structure

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { MONGO_URL } from "./variable.js";
+
+export const startDatabase = async () =>{
+   try {
+     await mongoose.connect(MONGO_URL);
+     console.log("connected to database");
+    } catch (error) {
+      console.log("not connectded to database", error.message);
+      console.log(typeof(MONGO_URL));
+      
+   }
+}
