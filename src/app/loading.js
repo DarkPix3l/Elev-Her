57bcbe5246b6f3987ae21585092c66155029b52f
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-07-02 12:42:12 +0200

    feat: add minimal loader

---
+++ src/app/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-screen h-screen bg-indigo-950 flex items-center justify-center">
+      <img src="/cat-loader.gif"/>
+    </div>
+  )
+}
