b7b2864d5a56828406a10e68132df2a091c6e162
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-07-05 18:25:01 +0200

    refactor: removed React Query due to slow loading and layout issues, move product fetching to Grid component, tried with server-side fetching approach.

--- src/components/product-grid/ProductGrid.jsx
+++ src/components/product-grid/ProductGrid.jsx
@@ -1,9 +1,11 @@
 import ProductCard from "./ProductCard";
+import { fetchProducts } from '@/services/product.apis';
 
-export default function ProductGrid({ products }) {
-
+export default async function ProductGrid() {
+  const products = await fetchProducts();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-8">
+      
       {products && products.map((product) => (
         <ProductCard key={product.slug} product={product} />
       ))}
