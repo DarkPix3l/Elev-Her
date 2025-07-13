b28c6025280cac1257a41e89565ff91a7d77ca68
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-07-14 00:24:41 +0200

    feat: dashboard routes protection.
    - Added protection logic to layout.js to wrap all dashboard pages and enforce auth.

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,14 +1,22 @@
+import { auth } from "@/app/auth";
+import { redirect } from "next/navigation";
 import Navbar from "@/components/ui/NavBar";
 import DashboardMenu from "@/components/UserDashboard/DashboardMenu";
 
-export default function DashboardLayout({ children }) {
+export default async function DashboardLayout({ children }) {
+  const session = await auth();
+
+  if (!session || !session.user) {
+    redirect("/"); // o una pagina di login
+  }
+
   return (
     <div className="min-h-screen [background-image:radial-gradient(farthest-corner_at_center,_oklch(0.6249_0.2197_356.35),_oklch(36.194%_0.03849_276.321)_60%)]">
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 py-25">
         <DashboardMenu />
-        
-        {children}</main>
+        {children}
+      </main>
     </div>
   );
 }
