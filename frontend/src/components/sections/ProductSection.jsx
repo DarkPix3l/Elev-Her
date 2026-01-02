932778f9d0f6c623562b14df51de3c54954c4e6a
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-01-02 19:34:32 +0100

    chore(ProductSection): remove unused pagination and filter menu code

--- frontend/src/components/sections/ProductSection.jsx
+++ frontend/src/components/sections/ProductSection.jsx
@@ -1,15 +1,5 @@
-/* import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"; */
-
-import ProductGrid from '../product-grid/ProductGrid';
-import Styles from './ProductSection.module.css';
+import ProductGrid from '../product-grid/ProductGrid'
+import Styles from './ProductSection.module.css'
 
 export default function ProductSection() {
   return (
@@ -21,45 +11,14 @@ export default function ProductSection() {
               Explore the latest models
             </h2>
             <p className="">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et{' '}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+              et{' '}
             </p>
           </div>
 
-          {/* temp filter menu */}
-          <div className="z-1 temp_menu overflow-x-scroll lg:overflow-x-visible max-w-full ">
-            <ul className="flex p-4 lg:p-7">
-              <li>Menu item 1</li>
-              <li>Menu item 2</li>
-              <li>Menu item 3</li>
-              <li>Menu item 4</li>
-            </ul>
-          </div>
-
           <ProductGrid />
-
-          {/*           <Pagination>
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious href="#" />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">1</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">2</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-
-              <PaginationItem>
-                <PaginationNext href="#" />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination> */}
         </div>
       </div>
     </section>
-  );
+  )
 }
